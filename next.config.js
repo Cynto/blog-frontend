5b4586d913d973a68889c6ede2e4f33921cb163a
Cynto <lucagaravello@outlook.com>
2022-05-05 23:55:23 +0200

    Add allowed image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['uc566fb39cfe356c36fbd3d6fe36.previews.dropboxusercontent.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
