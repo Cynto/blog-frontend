a2993495a7752a8a2457ee80688d72a0a04fe82a
Cynto <lucagaravello@outlook.com>
2022-05-08 18:16:14 +0200

    Add domain for create post background url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['uc566fb39cfe356c36fbd3d6fe36.previews.dropboxusercontent.com'],
+    domains: [
+      'uc566fb39cfe356c36fbd3d6fe36.previews.dropboxusercontent.com',
+      'uca3892d2045b4c59e30bd55644f.previews.dropboxusercontent.com',
+    ],
   },
 };
 
