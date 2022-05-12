19862bb876f7a313b2e623328b7383c8163d0514
Cynto <lucagaravello@outlook.com>
2022-05-12 20:49:09 +0200

    Add jest testing packages

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+const nextJest = require('next/jest');
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  automock: false,
+  globals: {
+    localStorage: new LocalStorageMock(),
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
