2f31ede4cf84d948e48fc4416dd27748f01901c2
Cynto <lucagaravello@outlook.com>
2022-05-17 18:31:24 +0200

    Create logout page instead of component

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -2,21 +2,18 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/LogoutForm.module.scss';
 
-const LogoutForm = (props: { setViewLogout: Function }) => {
+const Logout = () => {
   const router = useRouter();
-  const { setViewLogout } = props;
   return (
-    <div className={styles.totalContainer} data-testid="logout-form">
+    <div className={styles.totalContainer}>
       <div className={styles.background} />
-      <div className={styles.logoutForm}>
+      <div
+        className={styles.logoutForm}
+        data-testid="logout-form"
+        id="logout-form"
+      >
         <h1>Logout</h1>
-        <button
-          onClick={() => {
-            setViewLogout(false);
-          }}
-        >
-          Cancel
-        </button>
+        <button onClick={() => router.back()}>Cancel</button>
         <button
           onClick={() => {
             localStorage.removeItem('token');
@@ -31,4 +28,4 @@ const LogoutForm = (props: { setViewLogout: Function }) => {
   );
 };
 
-export default LogoutForm;
+export default Logout;
