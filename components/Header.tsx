bcc9525e4814b5f9d4369439d8c9b2da1e6dc31d
Cynto <lucagaravello@outlook.com>
2022-05-17 18:35:08 +0200

    Create basic header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.scss';
+import Head from 'next/head';
 
-const Header = (props: { setViewLogout: Function }) => {
-  const { setViewLogout } = props;
+const Header = (props: {
+  userObj: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    loggedIn: boolean;
+  };
+}) => {
+  const { userObj } = props;
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className={styles.header}>
-      <h1>Blog</h1>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/posts">
-        <a>Posts</a>
-      </Link>
-      <Link href="/posts/create">
-        <a>Create Post</a>
-      </Link>
-      <span onClick={() => setViewLogout(true)} data-testid="header-logout">
-        Logout
-      </span>
-    </header>
+    <>
+      <header className={styles.header}>
+        <h1>Bloggy</h1>
+        <div className={styles.menuNavContainer}>
+          <span className="material-symbols-outlined">menu</span>
+          <nav>
+            <Link href="/">
+              <a
+                className={
+                  currentRoute === '/' || currentRoute === ''
+                    ? styles.active
+                    : ''
+                }
+              >
+                Home
+              </a>
+            </Link>
+            {userObj.loggedIn ? (
+              <>
+                <Link href="/posts">
+                  <a className={currentRoute === '/posts' ? styles.active : ''}>
+                    Posts
+                  </a>
+                </Link>
+                <Link href="/posts/create">
+                  <a
+                    className={
+                      currentRoute === '/posts/create' ? styles.active : ''
+                    }
+                  >
+                    Create Post
+                  </a>
+                </Link>
+                <Link href="/logout">
+                  <a>Logout</a>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link href="/login">
+                  <a>Login</a>
+                </Link>
+                <Link href="/register">
+                  <a>Register</a>
+                </Link>
+              </>
+            )}
+          </nav>
+        </div>
+      </header>
+    </>
   );
 };
 
