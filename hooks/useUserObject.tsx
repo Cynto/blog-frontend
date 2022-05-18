74cd795438d0fcd2cf5ef096e026158299fb122c
Cynto <lucagaravello@outlook.com>
2022-05-18 18:21:16 +0200

    Move userObject to custom hook instead of context

---
+++ hooks/useUserObject.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import UserObjInterface from '../shared/interfaces/userObj.interface';
+
+const useUserObject = () => {
+  const [userObj, setUserObj] = useState<UserObjInterface | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch('http://localhost:4000/user', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+        const user = await res.json();
+
+        setUserObj(user);
+      } catch (error: any) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  return { userObj, loading, error };
+};
+
+export default useUserObject;
