0175c11e0a99a9dbf8b742002eb9b89a84db8cac
Cynto <lucagaravello@outlook.com>
2022-05-24 23:55:08 +0200

    Create basic register page tests

---
+++ __tests__/register.test.tsx
@@ -0,0 +1,68 @@
+require('jest-fetch-mock').enableMocks();
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import useUserObject from '../hooks/useUserObject';
+import { Router, useRouter } from 'next/router';
+import Register from '../pages/register';
+import { act } from 'react-dom/test-utils';
+
+const userObj = {
+  _id: '1',
+  firstName: 'John',
+  lastName: 'Doe',
+  loggedIn: true,
+};
+jest.mock('../hooks/useUserObject');
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn(),
+}));
+
+describe('Register page tests', () => {
+  it('renders without crashing', async () => {
+    useUserObject.mockReturnValue({
+      userObj: null,
+    });
+
+    render(<Register />);
+  });
+  it('renders input boxes and submit button ', async () => {
+    useUserObject.mockReturnValue({
+      userObj: null,
+    });
+
+    render(<Register />);
+
+    expect(screen.getByLabelText('First Name')).toBeInTheDocument();
+    expect(screen.getByLabelText('Last Name')).toBeInTheDocument();
+    expect(screen.getByLabelText('Email')).toBeInTheDocument();
+    expect(screen.getByLabelText('Password')).toBeInTheDocument();
+    expect(screen.getByLabelText('Confirm Password')).toBeInTheDocument();
+    expect(screen.getByText('Create Account')).toBeInTheDocument();
+  });
+  it('renders navigation links', () => {
+    useUserObject.mockReturnValue({
+      userObj: null,
+    });
+
+    render(<Register />);
+
+    expect(screen.getByText('Login')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
+  it('redirects to homepage if user is logged in', async () => {
+    useUserObject.mockReturnValue({
+      userObj,
+    });
+    const mockRouter = {
+      push: jest.fn(),
+    };
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+    render(<Register />);
+
+    await waitFor(() => {
+      expect(mockRouter.push).toHaveBeenCalledWith('/');
+    });
+  });
+});
