dbc3dae2b1b66851e8c2fa341efeaa69b5ce5d32
Cynto <lucagaravello@outlook.com>
2022-05-24 23:55:34 +0200

    Move some tests to seperate files

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,6 +3,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import Home from '../pages/index';
+import useUserObject from '../hooks/useUserObject';
+import { Router, useRouter } from 'next/router';
+
+const userObj = {
+  _id: '1',
+  firstName: 'John',
+  lastName: 'Doe',
+  loggedIn: true,
+};
 
 jest.mock('next/router', () => ({
   useRouter() {
@@ -21,65 +30,21 @@ jest.mock('next/router', () => ({
     };
   },
 }));
+jest.mock(
+  'next/link',
+  () =>
+    ({ children }) =>
+      children
+);
+jest.mock('../hooks/useUserObject');
 
-describe('Fetch', () => {
-  beforeEach(() => {
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        data: {
-          _id: '1',
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'john@gmail.com',
-          createdAt: '2020-01-01',
-          lastLogin: '2020-01-01',
-          __v: 0,
-        },
-      })
-    );
-  });
-  it('fetch is called once', () => {
-    render(<Home />);
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
-  it('fetch is called with the correct url', () => {
-    render(<Home />);
-    expect(fetch).toHaveBeenCalledWith('http://localhost:4000/user', {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer null',
-        'Content-Type': 'application/json',
-      },
-    });
-  });
-  it('fetch is called with the correct access token', () => {
-    localStorage.setItem('token', '123');
-    render(<Home />);
-    expect(fetch).toHaveBeenCalledWith('http://localhost:4000/user', {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer 123',
-        'Content-Type': 'application/json',
-      },
-    });
-  });
-});
+
 
 describe('Home', () => {
   beforeEach(() => {
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        data: {
-          _id: '1',
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'john@gmail.com',
-          createdAt: '2020-01-01',
-          lastLogin: '2020-01-01',
-          __v: 0,
-        },
-      })
-    );
+    useUserObject.mockReturnValue({
+      userObj,
+    });
   });
 
   it('renders without crashing', () => {
@@ -87,46 +52,6 @@ describe('Home', () => {
   });
   it('renders the header', () => {
     render(<Home />);
-    expect(screen.getByText('Blog')).toBeInTheDocument();
+    expect(screen.getByText('Bloggy')).toBeInTheDocument();
   });
 });
-
-describe('Logout', () => {
-  beforeEach(() => {
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        data: {
-          _id: '1',
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'john@gmail.com',
-          createdAt: '2020-01-01',
-          lastLogin: '2020-01-01',
-          __v: 0,
-        },
-      })
-    );
-  });
-  it('renders logout form on click', async () => {
-    render(<Home />);
-    const logout = screen.getByTestId('header-logout');
-    await userEvent.click(logout);
-
-    expect(screen.getByTestId('logout-form')).toBeInTheDocument();
-  });
-  it('deletes token on logout', async () => {
-    render(<Home />);
-    localStorage.setItem('token', '123');
-    const logout = screen.getByTestId('header-logout');
-    await userEvent.click(logout);
-    const logoutForm = screen.getByTestId('logout-form');
-    const logoutButton = screen.getByTestId('logout-button');
-    await userEvent.click(logoutButton);
-    expect(localStorage.getItem('token')).toBeNull();
-  });
-  
-}
-);
-    
-    
-
