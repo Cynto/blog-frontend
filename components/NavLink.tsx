52babd959e5c00f5531b346bd3276913e700096a
Cynto <lucagaravello@outlook.com>
2022-06-01 23:56:59 +0200

    Add active class styles to navigation links

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 const NavLink = (props: { href: string; text: string }) => {
   const { href, text } = props;
+  const [activeClass, setActiveClass] = useState<string>('');
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  useEffect(() => {
+    if (currentRoute === href) {
+      setActiveClass(
+        "after:scale-x-100 after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 dark:after:bg-slate-50 after:bg-slate-900"
+      );
+    }
+  }, [currentRoute, href]);
+
   return (
     <>
       <Link href={href}>
-        <a className="text-xl font-normal relative text-slate-900 dark:text-slate-100 hover:after:scale-x-100 hover:after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 dark:after:bg-slate-50 after:bg-slate-900 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out">
+        <a
+          className={`text-xl font-normal relative text-slate-900 dark:text-slate-100 hover:after:scale-x-100 hover:after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 dark:after:bg-slate-50 after:bg-slate-900 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out ${activeClass}`}
+        >
           {text}
         </a>
       </Link>
