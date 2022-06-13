a56280b36f7be57bed57a56784187974fee4465b
Cynto <lucagaravello@outlook.com>
2022-06-13 20:54:56 +0200

    Seperate navbar into own component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Header.module.scss';
 import Head from 'next/head';
 import UserObjInterface from '../shared/interfaces/userObj.interface';
 import useDarkMode from '../hooks/useDarkMode';
-import NavLink from './NavLink';
+import NavBar from './NavBar';
 
 const Header = (props: { userObj: UserObjInterface | null | false }) => {
   const { userObj } = props;
@@ -34,39 +34,7 @@ const Header = (props: { userObj: UserObjInterface | null | false }) => {
     width >= 968 ? setMenuOpen(false) : null;
   }, [width]);
 
-  const navLinks = [
-    {
-      href: '/',
-      text: 'Home',
-      visible: true,
-    },
-    {
-      href: '/posts',
-      text: 'Posts',
-      visible: true,
-    },
-    {
-      href: '/posts/create',
-      text: 'Create Post',
-      visible: userObj && userObj.isAdmin ? true : false,
-    },
-    {
-      href: '/logout',
-      text: 'Logout',
-      visible: userObj ? true : false,
-    },
-    {
-      href: '/login',
-      text: 'Login',
-      visible: userObj ? false : true,
-    },
-    {
-      href: '/register',
-      text: 'Register',
-      visible: userObj ? false : true,
-    },
-    {},
-  ];
+  
 
   return (
     <>
@@ -80,27 +48,7 @@ const Header = (props: { userObj: UserObjInterface | null | false }) => {
           Bloggy
         </h1>
         <div className="flex items-center gap-5 xl:justify-self-start relative">
-          <nav
-            className={` md:!opacity-100 md:!items-center md:!bottom-0
-               dark:bg-gray-900 bg-white px-0 py-0 my-0 md:!grid md:!relative grid-flow-col gap-x-8 ${
-                 menuOpen
-                   ? 'opacity-100 !grid auto-rows-min gap-10  justify-center justify-items-center fixed !grid-flow-row right-0 bottom-0 left-0 top-[13%]   shadow-lg border-t-2  border-gray-200 dark:border-gray-600 transition-all duration-500 ease-in-out origin-left'
-                   : 'opacity-0 absolute bottom-[999px] '
-               }`}
-          >
-            {navLinks.map((navLink, index) => {
-              if (navLink.visible) {
-                return (
-                  <NavLink
-                    key={index}
-                    href={navLink.href}
-                    text={navLink.text}
-                    menuOpen={menuOpen}
-                  />
-                );
-              }
-            })}
-          </nav>
+          <NavBar menuOpen={menuOpen} userObj={userObj} />
           {darkMode ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
---
+++ components/NavBar.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import NavLink from './NavLink';
+import UserObjInterface from '../shared/interfaces/userObj.interface';
+
+const NavBar = (props: {
+  menuOpen: boolean;
+  userObj: UserObjInterface | null | false;
+}) => {
+  const { menuOpen, userObj } = props;
+
+  const navLinks = [
+    {
+      href: '/',
+      text: 'Home',
+      visible: true,
+    },
+    {
+      href: '/posts',
+      text: 'Posts',
+      visible: true,
+    },
+    {
+      href: '/posts/create',
+      text: 'Create Post',
+      visible: userObj && userObj.isAdmin ? true : false,
+    },
+    {
+      href: '/logout',
+      text: 'Logout',
+      visible: userObj ? true : false,
+    },
+    {
+      href: '/login',
+      text: 'Login',
+      visible: userObj ? false : true,
+    },
+    {
+      href: '/register',
+      text: 'Register',
+      visible: userObj ? false : true,
+    },
+    {},
+  ];
+  return (
+    <nav
+      className={` md:!opacity-100 md:!items-center md:!bottom-0
+               dark:bg-gray-900 bg-white px-0 py-0 my-0 md:!grid md:!relative grid-flow-col gap-x-8 ${
+                 menuOpen
+                   ? 'opacity-100 !grid auto-rows-min gap-10  justify-center justify-items-center fixed !grid-flow-row right-0 bottom-0 left-0 top-[13%]   shadow-lg border-t-2  border-gray-200 dark:border-gray-600 transition-all duration-500 ease-in-out origin-left'
+                   : 'opacity-0 absolute bottom-[999px] '
+               }`}
+    >
+      {navLinks.map((navLink, index) => {
+        if (navLink.visible) {
+          return (
+            <NavLink
+              key={index}
+              href={navLink.href}
+              text={navLink.text}
+              menuOpen={menuOpen}
+            />
+          );
+        }
+      })}
+    </nav>
+  );
+};
+
+export default NavBar;
