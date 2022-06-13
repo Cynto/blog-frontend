703a2f80c8606c47011e3ad8e9778ed79812a57f
Cynto <lucagaravello@outlook.com>
2022-06-13 18:28:53 +0200

    Add responsive tailwind classes

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const NavLink = (props: { href: string; text: string }) => {
-  const { href, text } = props;
+const NavLink = (props: { href: string; text: string, menuOpen: boolean }) => {
+  const { href, text, menuOpen } = props;
   const [activeClass, setActiveClass] = useState<string>('');
+  const [menuClass, setMenuClass] = useState<string>('');
   const router = useRouter();
   const currentRoute = router.pathname;
 
   useEffect(() => {
     if (currentRoute === href) {
-      console.log('first')
       setActiveClass(
         "after:scale-x-100 after:origin-bottom-left after:content-{''} after:absolute after:w-full after:h-0.5 after:bottom-0 after:left-0 dark:after:bg-slate-50 after:bg-slate-900"
       );
@@ -19,13 +19,19 @@ const NavLink = (props: { href: string; text: string }) => {
         "hover:after:scale-x-100 hover:after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 dark:after:bg-slate-50 after:bg-slate-900 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out"
       );
     }
-  }, [currentRoute, href]);
+    if(menuOpen) {
+      setMenuClass(
+        "md:first:mt-0 first:mt-10 w-min"
+      )
+
+    }
+  }, [currentRoute, href, menuOpen]);
 
   return (
     <>
       <Link href={href}>
         <a
-          className={`text-xl font-normal relative text-slate-900 dark:text-slate-100 ${activeClass}`}
+          className={`text-2xl font-normal relative text-slate-900 dark:text-slate-100 ${activeClass} ${menuClass}`}
         >
           {text}
         </a>
