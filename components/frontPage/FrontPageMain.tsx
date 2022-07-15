7304657a122408c5be26fbbece09daaaf6fd6ec4
Cynto <lucagaravello@outlook.com>
2022-07-15 18:45:37 +0200

    Change components to dynamic imports

--- components/frontPage/FrontPageMain.tsx
+++ components/frontPage/FrontPageMain.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import FrontMidSection from './FrontMidSection';
+import dynamic from 'next/dynamic';
+const FrontMidSection = dynamic(() => import('./FrontMidSection'), {
+  ssr: false,
+});
 import blogPostObjInterface from '../../shared/interfaces/blogPostObj.interface';
+const StandardSmallArticlesContainer = dynamic(
+  () => import('./StandardSmallArticlesContainer'),
+  {
+    ssr: false,
+  }
+);
+const FrontBottomSection = dynamic(() => import('./FrontBottomSection'), {
+  ssr: false,
+});
 
 const FrontPageMain = ({ posts }: { posts: blogPostObjInterface[] }) => {
   return (
-    <div className="w-full  p-14 dark:bg-gray-900">
+    <div className="w-full  p-14 dark:bg-gray-900 grid gap-y-10">
       <FrontMidSection posts={posts} />
+      <StandardSmallArticlesContainer posts={posts} mid={false} />
+      <FrontBottomSection posts={posts} />
     </div>
   );
 };
