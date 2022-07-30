eab23ec0fe44c887d6b67670b03f9c279a921198
Cynto <lucagaravello@outlook.com>
2022-07-30 19:52:16 +0200

    Move comment form logic into own component

--- components/articlePage/CommentSection.tsx
+++ components/articlePage/CommentSection.tsx
@@ -5,6 +5,7 @@ import userObjInterface from '../../shared/interfaces/userObj.interface';
 import Link from 'next/link';
 import commentInterface from '../../shared/interfaces/comment.interface';
 import TextareaAutosize from 'react-textarea-autosize';
+import CommentForm from './CommentForm';
 
 const CommentSection = ({
   post,
@@ -14,46 +15,10 @@ const CommentSection = ({
   userObj: userObjInterface | null | undefined;
 }) => {
   console.log(post);
-  const [errors, setErrors] = useState<
-    [
-      {
-        value: String;
-        msg: String;
-        param: String;
-        location: String;
-      }
-    ]
-  >([{ msg: '', value: '', param: '', location: '' }]);
+  
   const [comments, setComments] = useState<commentInterface[]>([]);
 
-  const handleSubmit = async (data: any) => {
-    console.log(data.comment.value);
-    const userComment = data.comment;
-    const response = await fetch(
-      `http://localhost:4000/posts/${post._id}/comments`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-        body: JSON.stringify({
-          content: userComment.value,
-          post: post._id,
-        }),
-      }
-    );
-    const json = await response.json();
-    console.log(json);
-
-    if (json.comment) {
-      userComment.value = '';
-      console.log('comment added');
-      getComments();
-    } else {
-      setErrors(json.errors);
-    }
-  };
+  
   const getComments = async () => {
     const response = await fetch(
       `http://localhost:4000/posts/${post._id}/comments`
@@ -102,35 +67,8 @@ const CommentSection = ({
             <span className="block text-xl mb-3">
               You&apos;re signed in, <b>{userObj.firstName}</b>
             </span>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit(e.target);
-              }}
-            >
-              <TextareaAutosize
-                className="w-full border-[1px] border-slate-500  resize-none p-2 outline-slate-500 dark:bg-slate-500"
-                maxLength={240}
-                placeholder="Write a comment..."
-                name="comment"
-              />
-              <button
-                type="submit"
-                className="mr-3 group dark:bg-white border-[1px] border-slate-900 dark:border-0 rounded px-4 py-1 text-slate-900 font-bold"
-              >
-                <span className="relative group-hover:after:scale-x-100 group-hover:after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-slate-900 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out">
-                  Post
-                </span>
-              </button>
-              <button
-                type="button"
-                className="ml-3 my-5  border-slate-900 dark:border-0 rounded    font-bold"
-              >
-                <span className="relative hover:after:scale-x-100 hover:after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-slate-900 dark:after:bg-slate-100 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out">
-                  Cancel
-                </span>
-              </button>
-            </form>
+            <CommentForm post={post} getComments={getComments} />
+            
           </div>
         )}
         <div className=" w-full  pt-10 border-2">
