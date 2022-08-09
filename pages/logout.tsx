b6d926b6c169dbbc151e8b2567dc62661d65dc8c
Cynto <lucagaravello@outlook.com>
2022-08-09 20:51:43 +0200

    Switch to tailwind classes

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import styles from '../styles/LogoutForm.module.scss';
 
 const Logout = () => {
   const router = useRouter();
   return (
-    <div className={styles.totalContainer}>
-      <div className={styles.background} />
+    <div className="flex min-h-full justify-center items-center absolute right-0 left-0 bottom-0 top-0 z-10">
+      <Image
+        src="/backgrounds/logout_background.jpg"
+        layout="fill"
+        objectFit="cover"
+        
+        alt="background"
+      />
       <div
-        className={styles.logoutForm}
+        className="grid justify-center p-8 z-10 bg-slate-100 dark:bg-gray-900 text-slate-800 dark:text-slate-100 shadow-2xl"
         data-testid="logout-form"
         id="logout-form"
       >
-        <h1>Logout</h1>
-        <button onClick={() => router.back()}>Cancel</button>
+        <h1 className="text-3xl font-header justify-self-center">Logout</h1>
         <button
+          className="text-xl font-normal py-3 px-16 mb-3 border-2 rounded hover:bg-slate-200 dark:bg-slate-100 dark:hover:bg-slate-200 dark:text-slate-900 "
+          onClick={() => router.back()}
+        >
+          Cancel
+        </button>
+        <button
+          className="text-xl font-normal py-3 px-16 border-2 rounded hover:bg-slate-200 dark:bg-slate-100 dark:hover:bg-slate-200 dark:text-slate-900"
           onClick={() => {
             localStorage.removeItem('token');
             router.push('/login');
