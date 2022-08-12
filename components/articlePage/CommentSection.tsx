b8b1d180fb9cd1de2d83d64aef45220c466b6994
Cynto <lucagaravello@outlook.com>
2022-08-12 21:30:54 +0200

    Add text for when article has no comments

--- components/articlePage/CommentSection.tsx
+++ components/articlePage/CommentSection.tsx
@@ -33,9 +33,9 @@ const CommentSection = ({
   return (
     <div
       id="comments"
-      className="w-screen dark:bg-gray-900 dark:text-slate-100 flex justify-center max-w-screen"
+      className="w-full dark:bg-gray-900 dark:text-slate-100 flex justify-center max-w-screen"
     >
-      <div className="md:w-2/3 max-w-[630px]  border-t-2 grid grid-cols-1 justify-items-center p-5">
+      <div className="md:w-2/3 max-w-[630px] mt-10  border-t-2 grid grid-cols-1 justify-items-center p-5">
         <h3 className="dark:text-slate-100 text-2xl font-header ">Comments</h3>
 
         {!userObj ? (
@@ -72,16 +72,30 @@ const CommentSection = ({
             <CommentForm post={post} getComments={getComments} />
           </div>
         )}
-        <div className=" w-full  pt-10 border-2">
-          {comments.map((comment: commentInterface, index) => (
-            <SingleComment
-              comment={comment}
-              userObj={userObj}
-              getComments={getComments}
-              key={index}
-            />
-          ))}
-        </div>
+        {comments.length > 0 ? (
+          <div className=" w-full  pt-10 border-2">
+            <div>
+              
+            </div>
+            {comments.map((comment: commentInterface, index) => (
+              <SingleComment
+                comment={comment}
+                userObj={userObj}
+                getComments={getComments}
+                key={index}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="w-full text-center grid">
+            <span className="text-xl font-bold font-normal mb-3 mt-2">
+              No comments yet
+            </span>
+            <span className="font-normal">
+              Be the first to comment, reply and react
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
