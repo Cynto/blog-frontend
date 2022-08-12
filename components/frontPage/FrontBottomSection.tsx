fbecbe66747511dccf93f7611c6044c7ea79f885
Cynto <lucagaravello@outlook.com>
2022-08-12 21:29:57 +0200

    Move max articles logic to parent

--- components/frontPage/FrontBottomSection.tsx
+++ components/frontPage/FrontBottomSection.tsx
@@ -1,40 +1,55 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import blogPostObjInterface from '../../shared/interfaces/blogPostObj.interface';
 import NoPictureArticleShowcase from './NoPictureArticleShowcase';
 import BottomBigArticle from './BottomBigArticle';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
 
-const FrontBottomSection = ({ posts }: { posts: blogPostObjInterface[] }) => {
-  const { width } = useWindowDimensions();
-  const [maxArticles, setMaxArticles] = useState(0);
+const FrontBottomSection = ({
+  posts,
+  articleNumbers,
+}: {
+  posts: blogPostObjInterface[];
 
-  useEffect(() => {
-    if (width > 1500) {
-      setMaxArticles(2);
-    }
-    if (width <= 1500) {
-      setMaxArticles(2);
-    }
-    if (width <= 1200) {
-      setMaxArticles(1);
-    }
-    
-    console.log(maxArticles);
-  }, [width]);
+  articleNumbers: {
+    midStandard: number;
+    midNoPicture: number;
+    standardSmall: number;
+    bottomNoPicture: number;
+    bottomBig: number;
+  };
+}) => {
+  const { width } = useWindowDimensions();
+  let articlesShown = 0;
+
   return (
     <div className=" md:flex  gap-x-10">
       <div className="max-w-[300px] justify-self-end hidden md:grid auto-rows-min gap-y-5 ">
         {posts.map((post, index) => {
-          return index < 3 ? (
-            <NoPictureArticleShowcase key={index} post={post} mid={false} />
-          ) : null;
+          if (articlesShown < articleNumbers.bottomNoPicture) {
+            if (
+              index >=
+                articleNumbers.midStandard +
+                  articleNumbers.midNoPicture +
+                  articleNumbers.standardSmall &&
+              index <
+                articleNumbers.midStandard +
+                  articleNumbers.midNoPicture +
+                  articleNumbers.standardSmall +
+                  articleNumbers.bottomNoPicture
+            ) {
+              articlesShown++;
+              return (
+                <NoPictureArticleShowcase key={index} post={post} mid={false} />
+              );
+            }
+          }
         })}
       </div>
       <div className="flex-1 ">
         {posts.map((post, index) => {
-          return index < 1 ? (
-            <BottomBigArticle key={index} post={post} />
-          ) : null;
+          if (index === posts.length - 1) {
+            return <BottomBigArticle key={index} post={post} />;
+          }
         })}
       </div>
     </div>
