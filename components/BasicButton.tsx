1c3caf9e7ceef3f74d187884e7f3bbec16ed0bae
Cynto <lucagaravello@outlook.com>
2022-08-14 21:25:48 +0200

    Add ability to use callback with button

--- components/BasicButton.tsx
+++ components/BasicButton.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const BasicButton = ({ text }: { text: string }) => {
+const BasicButton = ({
+  text,
+  callback,
+}: {
+  text: string;
+  callback: Function;
+}) => {
+
   return (
-    <button className="group dark:bg-white border-[1px] border-slate-900 dark:border-0 rounded px-4 py-1 text-slate-900 font-bold">
+    <button
+      className="group dark:bg-white border-[1px] border-slate-900 dark:border-0 rounded px-4 py-1 text-slate-900 font-bold"
+      onClick={() => callback()}
+    >
       <span className="relative group-hover:after:scale-x-100 group-hover:after:origin-bottom-left after:content-{''} after:absolute after:w-full after:scale-x-0 after:h-0.5 after:bottom-0 after:left-0 after:bg-slate-900 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out">
         {text}
       </span>
