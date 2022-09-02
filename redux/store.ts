449365bf2374476bbc2c65009dcc0602adb53f94
Cynto <lucagaravello@outlook.com>
2022-09-02 18:09:41 +0200

    Create setupStore function to use in tests

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
 import userObjReducer from './slices/userObj';
 
 export const store = configureStore({
@@ -6,8 +7,20 @@ export const store = configureStore({
     userObj: userObjReducer,
   },
 });
+// Create the root reducer separately so we can extract the RootState type
+const rootReducer = combineReducers({
+  userObj: userObjReducer
+})
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof setupStore>;
