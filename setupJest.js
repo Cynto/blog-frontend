5d42d6248e6036ec0e288547c67abd1c51d14f19
Cynto <lucagaravello@outlook.com>
2022-09-20 17:16:54 +0200

    Add environment variables to tests

--- setupJest.js
+++ setupJest.js
@@ -1,1 +1,6 @@
-require('jest-fetch-mock').enableMocks()
+require('jest-fetch-mock').enableMocks();
+import { loadEnvConfig } from '@next/env';
+
+export default async () => {
+  loadEnvConfig(process.cwd());
+};
