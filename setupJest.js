6287b4f9fcdbcd13d61fd42f22f55b59b36e7563
Cynto <lucagaravello@outlook.com>
2022-09-23 18:40:03 +0200

    Add router mock for all tests

--- setupJest.js
+++ setupJest.js
@@ -4,3 +4,22 @@ import { loadEnvConfig } from '@next/env';
 export default async () => {
   loadEnvConfig(process.cwd());
 };
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      replace: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    };
+  },
+}));
