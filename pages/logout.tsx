7864b703fb9c371ec7d16dfe98a1cf51e940cc48
Cynto <lucagaravello@outlook.com>
2022-10-05 18:05:34 +0200

    Add title to page

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setUserObj } from '../redux/slices/userObj';
 import UserObjectComponent from '../components/UserObjectComponent';
 import DarkMode from '../components/DarkMode';
+import Head from 'next/head';
 
 const Logout = () => {
   const router = useRouter();
@@ -18,42 +19,47 @@ const Logout = () => {
   }, [userObj]);
 
   return (
-    <div className="flex min-h-full justify-center items-center absolute right-0 left-0 bottom-0 top-0 z-10">
+    <>
+      <Head>
+        <title>Bloggy Logout</title>
+      </Head>
       <UserObjectComponent />
       <DarkMode />
-      <Image
-        src="/backgrounds/logout_background.jpg"
-        layout="fill"
-        objectFit="cover"
-        alt="background"
-        priority
-      />
-      <div
-        className="grid justify-center p-8 z-10 bg-slate-100 dark:bg-gray-900 text-slate-800 dark:text-slate-100 shadow-2xl"
-        data-testid="logout-form"
-        id="logout-form"
-      >
-        <h1 className="text-3xl font-header justify-self-center">Logout</h1>
-        <button
-          className="text-xl font-normal py-3 px-16 mb-3 border-2 rounded hover:bg-slate-200 dark:bg-slate-100 dark:hover:bg-slate-200 dark:text-slate-900 "
-          onClick={() => router.back()}
+      <div className="flex min-h-full justify-center items-center absolute right-0 left-0 bottom-0 top-0 z-10">
+        <Image
+          src="/backgrounds/logout_background.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="background"
+          priority
+        />
+        <div
+          className="grid justify-center p-8 z-10 bg-slate-100 dark:bg-gray-900 text-slate-800 dark:text-slate-100 shadow-2xl"
+          data-testid="logout-form"
+          id="logout-form"
         >
-          Cancel
-        </button>
-        <button
-          className="text-xl font-normal py-3 px-16 border-2 rounded hover:bg-slate-200 dark:bg-slate-100 dark:hover:bg-slate-200 dark:text-slate-900"
-          onClick={() => {
-            localStorage.removeItem('token');
-            dispatch(setUserObj(null));
+          <h1 className="text-3xl font-header justify-self-center">Logout</h1>
+          <button
+            className="text-xl font-normal py-3 px-16 mb-3 border-2 rounded hover:bg-slate-200 dark:bg-slate-100 dark:hover:bg-slate-200 dark:text-slate-900 "
+            onClick={() => router.back()}
+          >
+            Cancel
+          </button>
+          <button
+            className="text-xl font-normal py-3 px-16 border-2 rounded hover:bg-slate-200 dark:bg-slate-100 dark:hover:bg-slate-200 dark:text-slate-900"
+            onClick={() => {
+              localStorage.removeItem('token');
+              dispatch(setUserObj(null));
 
-            router.push('/login');
-          }}
-          data-testid="logout-button"
-        >
-          Logout
-        </button>
+              router.push('/login');
+            }}
+            data-testid="logout-button"
+          >
+            Logout
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
