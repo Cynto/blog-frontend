1fcf79fe0c85e57a98a6af5c4ab834abac4db052
Cynto <lucagaravello@outlook.com>
2022-10-17 18:48:09 +0200

    Update Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  baseURL: "http://localhost:3000/",
-
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
-    },
-  },
-
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
+      // implement node event listeners here
     },
   },
 });
